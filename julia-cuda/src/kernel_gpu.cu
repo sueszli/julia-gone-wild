9be8c06014e2bbedc634df15f1c0e274bdc3f020
sueszli <jabaryyahya@gmail.com>
2024-05-25 20:20:01 +0200

    up

--- julia-cuda/src/kernel_gpu.cu
+++ julia-cuda/src/kernel_gpu.cu
@@ -5,8 +5,6 @@
 extern int global_block_x, global_block_y;
 
 void julia_kernel(float *julia_set, Complex c, float scale, int res_x, int res_y, int max_iter, float max_mag, float x_scale, float y_scale) {
-
-    // compute a good default block size
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
 
@@ -19,8 +17,8 @@ void julia_kernel(float *julia_set, Complex c, float scale, int res_x, int res_y
     float scaledX = scale * x_scale * (float)(x - res_x / 2) / (res_x / 2);
     float scaledY = scale * y_scale * (float)(y - res_y / 2) / (res_y / 2);
 
+    // julia computation
     Complex z(scaledX, scaledY);
-
     int i = 0;
     for(i = 0; i < max_iter; i++) {
         z = z * z + c;
@@ -29,7 +27,7 @@ void julia_kernel(float *julia_set, Complex c, float scale, int res_x, int res_y
         }
     }
 
-    // calculate shade for julia set value
+    // calculate shade
     float juliaShade = ((float)i) / max_iter;
     julia_set[y * res_x + x] = juliaShade; // note the swapped indices due to row-major order in memory
 }
@@ -37,6 +35,5 @@ void julia_kernel(float *julia_set, Complex c, float scale, int res_x, int res_y
 void launch_julia_kernel(float *d_julia_set, Complex c, float scale, int res_x, int res_y, int max_iter, float max_mag, float x_scale, float y_scale) {
     dim3 blockSize(16, 16); // example block size -> must be adjusted based on the GPU specs
     dim3 gridSize((res_x + blockSize.x - 1) / blockSize.x, (res_y + blockSize.y - 1) / blockSize.y);
-
     julia_kernel<<<gridSize, blockSize>>>(d_julia_set, c, scale, res_x, res_y, max_iter, max_mag, x_scale, y_scale);
 }
