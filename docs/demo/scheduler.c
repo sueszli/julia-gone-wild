c484d57874d9e0cf66bb6b5b3ff5256fd2aa408e
sueszli <jabaryyahya@gmail.com>
2024-05-26 01:06:43 +0200

    up

--- docs/demo/scheduler.c
+++ docs/demo/scheduler.c
@@ -3,19 +3,36 @@
 #include <omp.h>
 
 int main(int argc, char *argv[]) {
-    int nprocs = omp_get_num_procs();
-    printf("nprocs: %d\n", nprocs);
-    
+    int nenv = 3;
+    omp_set_num_threads(nenv); // set number of threads
+    printf("nenv: %d\n", nenv);
+
+    int chunk = 5;
+    omp_set_schedule(omp_sched_static, chunk);
+    // omp_set_schedule(omp_sched_dynamic, chunk);
+    // omp_set_schedule(omp_sched_guided, chunk);
+    printf("chunk size: %d\n", chunk);
+
     int i = 0;
     int n = 17;
     int a[n];
-    int t[nprocs];
+    int t[nenv];
     
     #pragma omp parallel for schedule(runtime)
     for (i=0; i<n; i++) {
-        a[i] = omp_get_thread_num();
-        t[omp_get_thread_num()]++;
-        printf("a[%d] = %d\n", i, a[i]);
-        printf("t[%d] = %d\n", omp_get_thread_num(), t[omp_get_thread_num()]);
+        a[i] = omp_get_thread_num(); // chosen thread per iteration
+        t[omp_get_thread_num()]++; // parallel increment
     }
+
+    printf("a (schedule): ");
+    for (i=0; i<n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    
+    printf("t (counter): ");
+    for (i=0; i<nenv; i++) {
+        printf("%d ", t[i]);
+    }
+    printf("\n");
 }
